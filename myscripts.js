2e37651d81394434ff84104de92849bdac9ad52a
CGbhakt420 <sanchit.kr007@gmail.com>
2024-07-17 18:16:14 +0530

    made a function which plays a round of rock paper scissors and computes the score of user and computer

--- myscripts.js
+++ myscripts.js
@@ -10,3 +10,36 @@ function getHumanChoice(){
     return move;
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+
+    if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors") {
+
+        if (humanChoice === computerChoice) {
+            console.log("It's a tie! Go again");
+            playRound();
+        } else if (humanChoice === "rock" && computerChoice === "paper") {
+            console.log("You lose "+computerChoice+" beats "+humanChoice);
+            computerScore++;
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
+            console.log("You lose "+computerChoice+" beats "+humanChoice);
+            computerScore++;
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
+            console.log("You lose "+computerChoice+" beats "+humanChoice);
+            computerScore++;
+        }else {
+            console.log("You win "+humanChoice+" beats "+computerChoice);
+            humanScore++;
+        }
+    }else {
+        console.log("Bad Inputs");
+        playRound();
+    }
+}
+
+
+
